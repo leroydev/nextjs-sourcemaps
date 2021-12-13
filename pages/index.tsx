88f765f39d3f472231b993bcd2f7a5f2d6d7f3c6
Leroy Witteveen <lwitteveen@thinkwisesoftware.com>
2021-12-13 21:10:40 +0100

    Added example which causes sourcemaps to fail

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import { LogSomeText } from './LogSomeText';
 
 const Home: NextPage = () => {
+  LogSomeText('test');
+
   return (
     <div className={styles.container}>
       <Head>
